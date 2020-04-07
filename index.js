e9abf66d950580c052d8604f16b8f3278b55cfb1
KrasimirHG <49240591+KrasimirHG@users.noreply.github.com>
2020-04-07 17:17:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,37 @@ window.onload = function() {
 	const cardHolder = document.getElementById("card-holder");
 	const cardNumber = document.getElementById("card-number");
 	const nomerCarta = document.querySelector(".nomer-carta");
+	
 
 	const handleChangeName = function(e) {
 		cardHolder.innerHTML = e.target.value.toUpperCase();
 	};
 
+	const handleChangeNumber = function(e) {
+		nomerCarta.innerHTML = e.target.value;
+
+		var cardNo = /^(?:3[47][0-9]{13})$/;
+		const vidKarta = document.getElementById("vid-carta");
+		console.log(vidKarta);
+		if(cardNumber.value.match(cardNo)) {
+			// vidKarta.style = "border:none;"
+			// vidKarta.innerHTML="";
+			document.getElementById("AE").style.display = "block"
+		}
+		else {
+			vidKarta.innerHTML = "BBBBB"
+		}
+	}
+	
+
 	cardName.addEventListener("input", handleChangeName);
+	cardNumber.addEventListener("input",handleChangeNumber);
+
+	// function numberChecker() {
+	// 	var cardNo = /^(?:3[47][0-9]{13})$/;
+	// 	if(cardNumber.value.match(cardNo)) {
+	// 		document.querySelector(".vid-karta").innerHTML = "AE"
+	// 	}
+	// 	else document.querySelector(".vid-karta").innerHTML = "BBBBB"
+	// }
 };
